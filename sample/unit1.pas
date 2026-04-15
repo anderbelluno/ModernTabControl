85fb7bbff7f5a8103928208af32ffe591a1300f4
Anderson Fiori <ander1.belluno@gmail.com>
2026-04-14 21:55:54 -0300

    Add sample

---
+++ sample/unit1.pas
@@ -0,0 +1,37 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ModernTabControl;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    ImageList1: TImageList;
+    ModernTabControl1: TModernTabControl;
+    Notebook1: TNotebook;
+    Page1: TPage;
+    Page2: TPage;
+    Page3: TPage;
+    Page4: TPage;
+    Page5: TPage;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
